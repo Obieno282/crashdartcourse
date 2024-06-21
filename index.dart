adb6b18b47b92109a5ea3a617ffd1d5a66337b85
Stella Obieno <obienostella@gmail.com>
2024-06-20 20:33:49 -0400

    This is my followup practice on dart programming

---
+++ index.dart
@@ -0,0 +1,95 @@
+void main() {
+  //data types
+  //int number whole number
+  //double> decimal number
+  /*
+ sdfhtyreeqdvfhukplmnbc
+ */
+  //int age = 44;
+  // print(a
+  // const name = "Wesley";
+  //final country = "Kenya";
+  //print(name);
+  // print(age);
+  // print("Hello, my name is $name and I am $age years old and am from $country");
+  // var age = 150;
+  //var age2 = 30;
+  //var age3 = 100;
+  //print(age > age3);
+
+  //conditional statements
+  //if statement
+  /*
+  var people = 10;
+  if (people < 3) {
+    print("The session cannot continue");
+  }
+  else {
+    print("Lesson will continue");
+  }
+  */
+  //if else if statement
+  /*
+  var marks = 70;
+  if (marks >=80 && marks <=100){
+    print("A");
+  }
+  else if(marks >= 70 && marks <70){
+    print("You got grade B of $marks marks");
+    }
+    else if(marks >=60 && marks <60){
+      print("C");
+    }
+      else{
+        print("Invalid marks");
+      }
+    }
+  */
+
+  //switch statement
+  //switch(variable){case value :break; default:}
+  /*
+  var marks = 70;
+  switch(marks) {
+    case 80:
+    print("A");
+    break;
+    case 70:
+    print("B");
+    break;
+    case 60:
+    print("C");
+    break;
+    case 50:
+    print("D");
+    break;
+    case 40:
+    print("E");
+    break;
+    default:
+    print("Invalid Marks");
+  }
+}
+*/
+
+  var vowel = "A";
+  switch (vowel) {
+    case "A":
+      print("vowel A");
+      break;
+    case "E":
+      print("vowel E");
+      break;
+    case "I":
+      print("vowel I");
+      break;
+    case "O":
+      print("vowel O");
+      break;
+    case "U":
+      print("vowel U");
+      break;
+    default:
+      print("Invalid vowel");
+  }
+}
